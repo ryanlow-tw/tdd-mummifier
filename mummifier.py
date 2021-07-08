767392eabfb081f6eed51bdd33b1723d79375ebf
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-08 16:45:15 +0800

    add test to return mommy if string is a

--- mummifier.py
+++ mummifier.py
@@ -1,6 +1,10 @@
 class Mummifier:
 
     def mummify(self, input_string):
+
+        if input_string == 'a':
+            return "mommy"
+
         return input_string
 
 
--- test_mummifier.py
+++ test_mummifier.py
@@ -12,4 +12,8 @@ class MummifierTest(unittest.TestCase):
         result = Mummifier().mummify("str")
         self.assertEqual("str", result)
 
+    def test_should_return_a_if_string_is_a_vowel(self):
+        result = Mummifier().mummify("a")
+        self.assertEqual("mommy", result)
+
 
