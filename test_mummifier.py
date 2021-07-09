0c6e76203519cd1a39d7b5be5118c6a7458c877e
ryanlow-tw <ryan.low@thoughtworks.com>
2021-07-09 11:36:53 +0800

    remove comments

--- test_mummifier.py
+++ test_mummifier.py
@@ -41,9 +41,6 @@ class MummifierTest(unittest.TestCase):
         self.assertEqual("blmommy", result)
 
     def test_for_null_value(self):
-        # expected_exception = raise Exception("Please input a valid string!")
-        # self.assertRaises(expected_exception, Mummifier().mummify, None)
-
         with self.assertRaises(TypeError) as cm:
             Mummifier().mummify(None)
         the_exception = cm.exception
